e0fca303b13097159fd506fac4b256b903323029
Ngetich K Peter <47054134+NgetichKPeter@users.noreply.github.com>
2026-07-24 18:41:31 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabaseUrl = "https://ydagguthswsyhexrgwgb.supabase.co";
 
-const supabaseAnonKey = "sb_publishable_I7mg_iLZYJkGRgJ-ax0UKg_I9cX-BML";
+const supabaseAnonKey =
+"sb_publishable_I7mg_iLZYJkGRgJ-ax0UKg_I9cX-BML";
 
 export const supabase = createClient(
     supabaseUrl,
